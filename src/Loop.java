2d3dd2a00b928a4a1e85c89bd6c9e8c5a07c2447
javascriptsucks <evix1988@live.cn>
2023-07-10 01:04:23 -0600

    more loop syntax

--- src/Loop.java
+++ src/Loop.java
@@ -1,2 +1,33 @@
+import java.util.Scanner;
+
 public class Loop {
+    static public void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(i);
+        }
+        int count = 0;
+        while (count <= 5) {
+            count++;
+            System.out.println(count+ " Times Called");
+        }
+
+        String input = "";
+        Scanner scanner = new Scanner(System.in);
+        while (!input.equals("quit")) {
+            System.out.print("Your input is: ");
+            input = scanner.nextLine().toLowerCase().trim();
+            if (input.equals("pass")) continue;
+            if (input.equals("quit"))  break;
+
+            System.out.println("Input is: " + input);
+        }
+
+        do {
+            System.out.print("Your input is: ");
+            input = scanner.nextLine().toLowerCase().trim();
+            if (input.equals("pass")) continue;
+            if (input.equals("start"))  break;
+            System.out.println("Input is: " + input);
+        } while (!input.equals("start"));
+    }
 }
