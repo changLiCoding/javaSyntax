a81be92512cbe0f0bb48320a5606ac595d86e706
javascriptsucks <evix1988@live.cn>
2023-07-11 00:39:55 -0600

    oop set and get

--- src/OOP/OOPBasic.java
+++ src/OOP/OOPBasic.java
@@ -4,9 +4,13 @@ public class OOPBasic {
     public static void main(String[] args) {
 
 
-        Animal piggy = new Animal("piggy", 4);
+        Animal piggy = new Animal("piggy", 4, "corn");
 
         piggy.sayHello();
+        System.out.println(piggy.getName());
+        piggy.setName("Little Piggy");
+        System.out.println(piggy.getName());
+        piggy.eatFood();
 
     }
 }
@@ -14,12 +18,25 @@ public class OOPBasic {
  class Animal {
     private String name;
     private int legsNum;
+    private String food;
 
-    public Animal(String name, int legsNum) {
+    public Animal(String name, int legsNum, String food) {
         this.name = name;
         this.legsNum = legsNum;
+        this.food = food;
     }
     public void sayHello() {
-        System.out.print("Hello! My name is: " + this.name + " And I have " + legsNum + " legs. ");
+        System.out.println("Hello! My name is: " + this.name + " And I have " + legsNum + " legs. ");
     }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void eatFood() {
+         System.out.println("Me like eat " + this.food);
+     }
 }
