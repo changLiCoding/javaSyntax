0a3de28577783c28178520895b0548dd91edbfd5
javascriptsucks <evix1988@live.cn>
2023-07-12 17:21:11 -0600

    Java final keyword

---
+++ src/OOP/FinalKeyword.java
@@ -0,0 +1,28 @@
+package OOP;
+
+public class FinalKeyword {
+    public static void main(String[] args) {
+        VariableKeywords testVariable = new VariableKeywords("Chang");
+        System.out.println(testVariable.name);
+
+        VariableKeywordsChild child = new VariableKeywordsChild("childName");
+        child.test();
+    }
+}
+
+class VariableKeywords {
+    final public String name;
+
+    public VariableKeywords(String name) {
+        this.name = name;
+    }
+
+     protected final void test() {
+        System.out.println("Called method in parent. ");
+    }
+}
+class VariableKeywordsChild extends VariableKeywords {
+    public VariableKeywordsChild(String name) {
+        super(name);
+    }
+}
