231fe09b32ce271909394b241eb39b7c6beefeca
javascriptsucks <evix1988@live.cn>
2023-07-14 01:44:28 -0600

    string common method

--- src/ObjectInJava/StringInJava.java
+++ src/ObjectInJava/StringInJava.java
@@ -2,7 +2,7 @@ package ObjectInJava;
 
 public class StringInJava {
     public static void main(String[] args) throws Exception {
-        String name = "CHANGLI";
+        String name = "CHANG LI";
         char[] nameChar = {'c', 'h', 'a', 'n', 'g'};
         byte[] bs = {-28, -72, -83, -27, -101, -67};
 
@@ -18,6 +18,42 @@ public class StringInJava {
         System.out.println("a\tb");
         System.out.println("A\nB");
 
+        String s = "a" + "b";
+        String s1 = "ab";
+        String s2 = 1 + s1 + 2;
+        String s3 = s1 + 1 + 2;
+        String s4 = 1 + 2 + s1;
+        System.out.println(s.hashCode());
+        System.out.println(s1.hashCode());
+        System.out.println(s.equals(s1));
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
 
+        System.out.println(s4.concat(s3));
+
+        String name1 = "chang li";
+        System.out.println(name.equals(name1));
+        System.out.println(name.equalsIgnoreCase(name1));
+        int i = name.compareTo(name1);
+        System.out.println(i);
+
+        String surname = name.substring(5,name.length());
+        System.out.println(surname);
+        String firstName= name.substring(0, "CHANG".length());
+        System.out.println(firstName);
+
+        String[] nameWithSpace = name.split(" ");
+        System.out.println(nameWithSpace.length);
+        for (String s5 : nameWithSpace) {
+            System.out.println(s5);
+        }
+
+        String nameWithLotsOfSpaces = "     CHANG   LI      ";
+        System.out.println(nameWithLotsOfSpaces.trim());
+
+        String nameWithLowerFirst = nameWithLotsOfSpaces.replace("CHANG", "chang");
+        System.out.println(nameWithLowerFirst);
+        System.out.println(nameWithLotsOfSpaces.replaceAll("CHANG|LI", "replaced"));
     }
 }
